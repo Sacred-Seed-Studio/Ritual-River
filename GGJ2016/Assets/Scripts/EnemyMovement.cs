94c8ed66841ce9c27fa0a6fa0c513710a6caeb7c
Joel Kuntz <joel.kuntz.92@gmail.com>
2016-01-30 17:52:23 -0400

    UI added to main scene

--- GGJ2016/Assets/Scripts/EnemyMovement.cs
+++ GGJ2016/Assets/Scripts/EnemyMovement.cs
@@ -1,19 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class EnemyMovement : MonoBehaviour
 {
     public float speed = 1f;
+
     Rigidbody2D rb2d;
+    [HideInInspector]
+    public Animator anim;
 
     void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     public void Move(Vector2 input)
     {
+        anim.SetFloat("x", input.x);
+        anim.SetFloat("y", input.y);
+        anim.SetBool("Idle", false);
+
         Vector2 p = rb2d.position + input.normalized * speed * Time.deltaTime;
         rb2d.MovePosition(p);
     }
