5c38db1a50bf62aa088fa66198937a6b48360c41
Sarah MacDonald <Sarah MacDonald>
2016-01-29 21:16:41 -0400

    Working on the game flow.

---
+++ GGJ2016/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+    public static LevelLoader levelLoader;
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
