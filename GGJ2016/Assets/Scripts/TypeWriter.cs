437c8dfa470d3f91e399c0de3ff80256ef5e97ca
Joel Kuntz <joel.kuntz.92@gmail.com>
2016-01-30 14:51:06 -0400

    Set up typewrite effect and pause

---
+++ GGJ2016/Assets/Scripts/TypeWriter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TypeWriter : MonoBehaviour
+{
+    public string textToDisplay;
+    public float letterPause;
+    //public AudioClip letterSound;
+    public Text textBox;
+
+    void Start()
+    {
+        textBox.text = "";
+    }
+
+    void OnEnable()
+    {
+        Debug.Log("OnEnable");
+        Debug.Assert(textBox != null && textToDisplay != null, "Something not initialized");
+        StartCoroutine(TypeText());
+    }
+
+    void OnDisable()
+    {
+        textBox.text = "";
+    }
+
+    IEnumerator TypeText()
+    {
+        Debug.Log("Top of typetext");
+        foreach (char letter in textToDisplay.ToCharArray())
+        {
+            Debug.Log("Top of typetext loop");
+            textBox.text += letter;
+            yield return new WaitForSeconds(letterPause);
+        }
+    }
+}
